473ce2bb2a3d1908742af47137e63a0a50a358a1
lightmen <519542738@qq.com>
2015-10-07 22:59:57 +0800

    modify toolbox.c

--- toolbox/toolbox.c
+++ toolbox/toolbox.c
@@ -5,14 +5,13 @@ int main(int argc, char **argv);
 
 int toolbox_main(int argc, char **argv)
 {
-	int ret = 0;
 	if(argc > 1){
-		ret = main(argc-1,argv+1);
+		return main(argc-1,argv+1);
 	}else{
 		printf("toolbox\n");
+		return 0;
 	};
 
-	return ret;
 }
 
 struct {
@@ -26,29 +25,23 @@ struct {
 int main(int argc, char **argv)
 {
 	int i;
-	int ret;
 	char *name;
-	if(argc > 1){
-		name = argv[1];
-		argc--;
-		argv++;
-	}else{
-		name = argv[0];
-	}
 
-	name = strrchr(name,'/')+1;
+	name = strrchr(argv[0],'/');
+	if(name)
+		name++;
+	else
+		name = argv[0];
 
 	for(i = 0; tools[i].name; i++){
-		if(!strcmp(tools[i].name,name)){	
-			ret = tools[i].func(argc, argv); 
-			break;
+		if(!strcmp(tools[i].name,name)){
+			return tools[i].func(argc, argv);
 		}
 	}
 
 	if(!tools[i].name){
 		printf("tool %s not fountd\n",name);
-		ret = -1;
 	}
 
-	return ret;
+	return -1;
 }
