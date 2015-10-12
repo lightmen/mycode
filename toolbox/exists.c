ef08c30cd6df247d9af0b44cfb32eb35e5c11d1a
lightmen <519542738@qq.com>
2015-10-13 00:07:17 +0800

    modify exists.c

--- toolbox/exists.c
+++ toolbox/exists.c
@@ -4,10 +4,13 @@
 int exists_main(int argc, char **argv)
 {
 	struct stat st;
-	if(argc < 2)
+	if(argc < 2){
+		printf("Usage: exists file\n");
 		return 1;
+	}
 
 	if(stat(argv[1],&st) < 0){
+		printf("%s not exists\n",argv[1]);
 		return 1;
 	}else{
 		return 0;
